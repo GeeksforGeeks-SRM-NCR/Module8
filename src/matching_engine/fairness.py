060843dfbcc85d42748bbd6d76cc79610bf20960
Trinity <tintinyttttttt@gmail.com>
2026-02-18 15:06:21 +0530

    Update fairness.py

--- src/matching_engine/fairness.py
+++ src/matching_engine/fairness.py
@@ -1,6 +1,6 @@
 def counterfactual_score(base_score, group, protected):
 
-    if group in protected:
+    if isinstance(protected, (list, set, tuple)) and group in protected:
         return base_score + 0.25
 
     return base_score
